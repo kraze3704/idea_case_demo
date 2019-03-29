a28dfd374ab174c33946afcd280c58d25685bf95
changHyunKim <kraze3704@gmail.com>
2019-03-29 23:51:36 +0200

    adding react-router to the app
    package.json
    > npm audit fix command executed to resolve conflicts
    > install -s react-router-dom for adding Router to category view
    
    CategoryViews/Categories.jsx
    > instead of displaying both the complete list and filtered list of
    categories now displays only one
    > default is fetch_all, user can request the filtered list by clicking
    on the link in the navigation bar
    > React Router renders the requested view

--- src/Views/CategoryViews/Categories.jsx
+++ src/Views/CategoryViews/Categories.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import CategoryList from '../../Components/CategoryComponents/CategoryList';
 import CategoryAdd from '../../Components/CategoryComponents/CategoryAdd';
 import RandomizedCategory from '../../Components/CategoryComponents/RandomizedCategory';
@@ -15,7 +17,7 @@ export default class Categories extends Component {
     */
 
     render() {
-
+        /* older rendering without Router
         return (
             <div className="Container">
                 <CategoryAdd />
@@ -24,5 +26,25 @@ export default class Categories extends Component {
                 <CategoryFilterByBudgetLimit />
             </div>
         );
+        */
+       return(
+        <Router>
+            <CategoryAdd />
+
+            <div class="nav-bar">
+                <nav>
+                    <ul>
+                        <li><Link to="/">category list</Link></li>
+                        <li><Link to="/categoryFiltered">category filtered</Link></li>
+                    </ul>
+                </nav>
+
+                <Route path="/" exact component={CategoryList} />
+                <Route path="/categoryFiltered" component={CategoryFilterByBudgetLimit} />
+            </div>
+
+            <RandomizedCategory />
+        </Router>
+       );
     }
 }
